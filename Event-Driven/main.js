96cd7575bc4ad46ae76071403164a841d92e742a
Mohammedsalman14 <94598350+Mohammedsalman14@users.noreply.github.com>
2023-03-19 19:18:48 +0530

    Some ex...

--- Event-Driven/main.js
+++ Event-Driven/main.js
@@ -7,12 +7,19 @@ customEmiter.emit('testing',"Salman",14);
 function test(name,id){
     console.log(`hello world ${name} ${id}`);
 }
-// const http=require('http');
 
-// const server=http.createServer();
 
-// server.on('request',(req,res)=>{
 
-//    res.end("hello world");
+
+
+const http=require('http');
+
+const server=http.createServer();
+
+server.on('request',(req,res)=>{
+
+   res.end("hello world");
     
-// }).listen(8060);
+}).listen(8060);
+
+
