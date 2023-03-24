1d284e182e414e936d558ee0c2e9ccfa370dafa2
Mohammedsalman14 <94598350+Mohammedsalman14@users.noreply.github.com>
2023-03-24 12:16:39 +0530

    Adding with html in nodejs

--- Http/main.js
+++ Http/main.js
@@ -1,9 +1,25 @@
-const http=require('http');
+const http = require('http');
+const {readFileSync}=require('fs');
+const homePage=readFileSync('./index.html');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    // res.write("<h1>hello world</h1>");
+    console.log(req.rawHeaders);
+    if (req.url == "/about") {
+        res.writeHead(200, { 'content-type': 'text/html' });
 
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.write("<h1>hello world</h1>");
+        res.write(homePage);
+
+    } else if (req.url == "/error") {
+        res.writeHead(404, { 'content-type': 'text/html' });
+
+        res.write("<h1>Error</h1>");
+
+    }
     res.end();
+
+
 });
 
-server.listen(5000);
+server.listen(5001);
+
